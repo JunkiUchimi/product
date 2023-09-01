71debb95a30729e083331eb3952099eadf439250
EC2 Default User <ec2-user@ip-172-31-34-246.ec2.internal>
2023-09-01 18:56:46 +0900

    202309011

--- resources/views/posts/accordion.blade.php
+++ resources/views/posts/accordion.blade.php
@@ -12,12 +12,12 @@
         <button class="accordion-btn">近くにスーパーなどはありますか？</button>
     <div class="accordion-content">
         <p>徒歩10分圏内にイトーヨーカ堂があります。</p>
-    </div>
+    </div><br>
 
     <button class="accordion-btn">駐車スペースはありますか？</button>
     <div class="accordion-content">
         <p>現在１台分の駐車スペースが空いています。</p>
-    </div>
+    </div><br>
         
     </div>
     <script src="accordion.js"></script>
--- resources/views/posts/form.blade.php
+++ resources/views/posts/form.blade.php
@@ -2,7 +2,7 @@
 <html>
 
 <div class="container">
- <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfKtoHOohVTncp7D0UnmiyfpowandLOJWSG9qQ5tQ-QgsBiZg/viewform?embedded=true" width="640" height="953" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+ <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfKtoHOohVTncp7D0UnmiyfpowandLOJWSG9qQ5tQ-QgsBiZg/viewform?embedded=true" width="1450" height="953" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
   </div>
 
 </html>
