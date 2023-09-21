861ef3be17911f79fa465a8070ed81049b8d3dc0
EC2 Default User <ec2-user@ip-172-31-34-246.ec2.internal>
2023-09-21 14:08:26 +0900

    Authの変更

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,18 +14,18 @@ class PostController extends Controller
  * @params Object Post // 引数の$postはid=1のPostインスタンス
  * @return Reposnse post view
  */
-public function show(Post $post)
-{
-    return view('posts.show')->with(['post' => $post]);
- //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
-}
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
 
 
-public function index(Post $post)
-{
-    if(\Auth::id() == 1){
+    public function index(Post $post)
+    {
+    
     return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
-    } 
+    
     }
     public function create()
     {
